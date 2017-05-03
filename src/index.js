a23ef151a97a69650d26aa65b08915a096b3c238
BadWaka <1456683844@qq.com>
2017-05-03 20:45:46 +0800

    配置webpack和react和babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 /**
  * Created by BadWaka on 03/05/2017.
  */
-console.log('index');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello,World!</h1>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
+    devtool: 'cheap-eval-source-map',
     module: {
         rules: [
             // babel
