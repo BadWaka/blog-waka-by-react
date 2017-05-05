45e6ab501e682356f1922c908cf2f6513be6bb47
BadWaka <1456683844@qq.com>
2017-05-05 14:06:08 +0800

    作品已申请版保 样式完成

--- src/businessComponents/CopyrightProtectionModals/AlreadyAppleFor/AlreadyApplyFor.js
+++ src/businessComponents/CopyrightProtectionModals/AlreadyAppleFor/AlreadyApplyFor.js
@@ -17,38 +17,63 @@ export default class AlreadyApplyFor extends Component {
     static defaultProps = {};
 
     render() {
+
+        let isCopyrightRecords = false; // 是否有版保记录
+
         return (
             <Modal title="作品已申请版保">
-                <section className={style.row1}>
-                    <input type="radio" name="relateWay"/>
-                    <span className={style.text}>系统已列出您在版权保护中心的版保记录，请选择对应记录与作品进行关联</span>
-                    <div className={style.copyrightRecord}>
-                        <input type="radio" name="copyrightRecord"/>
-                        <span className={style.text}>查理一世</span>
-                        <span className={style.textRight}>宋晓明</span>
-                    </div>
-                    <div className={style.copyrightRecord}>
-                        <input type="radio" name="copyrightRecord"/>
-                        <span className={style.text}>查理二世</span>
-                        <span className={style.textRight}>宋晓明2</span>
-                    </div>
+                {/* 有版保记录显示这个 */}
+                {isCopyrightRecords &&
+                <section className={style.hasCopyrightRecords}>
+                    {/* 版保记录 */}
+                    <section className={style.row1}>
+                        <input type="radio" name="relateWay"/>
+                        <span className={style.text}>系统已列出您在版权保护中心的版保记录，请选择对应记录与作品进行关联</span>
+                        {/* 版保记录项 */}
+                        <div className={style.copyrightRecord}>
+                            <input type="radio" name="copyrightRecord"/>
+                            <span className={style.text}>查理一世</span>
+                            <span className={style.textRight}>宋晓明</span>
+                        </div>
+                        <div className={style.copyrightRecord}>
+                            <input type="radio" name="copyrightRecord"/>
+                            <span className={style.text}>查理二世</span>
+                            <span className={style.textRight}>宋晓明2</span>
+                        </div>
+                    </section>
+                    {/* 版保号关联 */}
+                    <section className={style.row2}>
+                        <input type="radio" name="relateWay"/>
+                        <span className={style.text}>通过版保关联号进行关联</span>
+                        {/* 通过版保号进行关联 */}
+                        <div className={style.copyrightNumber}>
+                            <div className={style.tips1}>如果系统没有找到您要的版保记录，您可以通过关联号进行手动关联</div>
+                            <input className={style.input} type="text" placeholder="版保关联号"/>
+                            {/* 正常提示 */}
+                            <div className={style.normalTips}>作品《查理一世》</div>
+                            {/* 错误提示 */}
+                            <div className={style.errorTips}>版保关联号已经关联了其他的作品</div>
+                        </div>
+                    </section>
                 </section>
-                <section className={style.row2}>
-                    <input type="radio" name="relateWay"/>
+                }
+                {/* 没有版保记录显示这个 */}
+                {!isCopyrightRecords &&
+                <section className={style.noCopyrightRecords}>
                     <span className={style.text}>通过版保关联号进行关联</span>
                     {/* 通过版保号进行关联 */}
                     <div className={style.copyrightNumber}>
                         <div className={style.tips1}>如果系统没有找到您要的版保记录，您可以通过关联号进行手动关联</div>
-                        <div>
-
-                        </div>
-                        <input className={style.input} type="text"/>
+                        <input className={style.input} type="text" placeholder="版保关联号"/>
                         {/* 正常提示 */}
                         <div className={style.normalTips}>作品《查理一世》</div>
                         {/* 错误提示 */}
                         <div className={style.errorTips}>版保关联号已经关联了其他的作品</div>
                     </div>
                 </section>
+                }
+
+                {/* 按钮栏 */}
                 <section className={style.row3}>
                     <span className={style.left}>作品未申请版保 ></span>
                     <span className={style.right}>
@@ -56,6 +81,7 @@ export default class AlreadyApplyFor extends Component {
                         <span className={style.cancel}>取消</span>
                     </span>
                 </section>
+
             </Modal>
         );
     }
