d64e6c6af88894454a2987a230510b8e6cce4982
BadWaka <1456683844@qq.com>
2017-05-05 10:34:34 +0800

    配置TypeScript并正确输出结果

---
+++ webpack.config.typescript.js
@@ -0,0 +1,91 @@
+/**
+ * Created by BadWaka on 03/05/2017.
+ */
+const path = require('path');
+
+module.exports = {
+    // 入口
+    entry: './src_ts/index.tsx',
+    // 出口
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public_ts')
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    // 模块
+    module: {
+        // 配置模块规则
+        rules: [
+            // ES6
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'source-map-loader!babel-loader',
+                }
+            },
+            // TypeScript
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: 'ts-loader',
+                }
+            },
+            // tsx
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: "awesome-typescript-loader"
+                }
+            },
+            // CSS
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings; style-loader 是让 javascript 认识css
+                }, {
+                    loader: 'css-loader?modules', // 在css-loader后面加上参数modules，表示打开CSS Modules功能
+                }]
+            },
+            // SCSS
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings; style-loader 是让 javascript 认识css
+                }, {
+                    loader: 'css-loader?modules' // translates CSS into CommonJS; css-loader 是将 css 装载到 javascript
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS; sass-loader 是将sass文件编译成css
+                }]
+            },
+            // img url-loader file-loader
+            {
+                test: /\.(png|gif|jpe?g|svg)$/i,
+                use: {
+                    loader: 'url-loader?limit=10000'   // 如果资源小于10kb就内联资源，否则就调用 file-loader 并关联它
+                }
+            }
+        ]
+    },
+    // 插件
+    plugins: [],
+    // 开发工具
+    devtool: 'source-map',
+    // dev server
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        compress: true,
+        port: 8080
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
