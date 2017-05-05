1d2592bd67476c907cd113ea3d6da0e335d5a4f9
BadWaka <1456683844@qq.com>
2017-05-05 10:56:21 +0800

    作品未申请版保

--- src/businessComponents/CopyrightProtectionModals/NotApplyFor/NotApplyFor.js
+++ src/businessComponents/CopyrightProtectionModals/NotApplyFor/NotApplyFor.js
@@ -7,21 +7,35 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import style from './style.scss';
+import freeDCIImg from './freeDCI.svg';
+import freeLawImg from './freeLaw.svg';
 
 import Modal from '../../Modal/Modal';
 
 export default class NotApplyFor extends Component {
 
-    static propTypes = {
-        title: PropTypes.string,    // 标题
-    };
+    static propTypes = {};
 
     static defaultProps = {};
 
     render() {
         return (
             <Modal title="作品未申请版保">
-                NotApplyFor
+                <section className={style.row1}>申请后可获得以下权益：</section>
+                <section className={style.row2}>
+                    <div className={style.free1}>
+                        <img src={freeDCIImg} alt=""/>
+                        <span className={style.text}>免费DCI证书</span>
+                    </div>
+                    <div className={style.free2}>
+                        <img src={freeLawImg} alt=""/>
+                        <span className={style.text}>免费法务咨询</span>
+                    </div>
+                </section>
+                <section className={style.row3}>
+                    <span>作品已申请版保 ></span>
+                    <button>马上去申请</button>
+                </section>
             </Modal>
         );
     }
