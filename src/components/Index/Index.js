b12a04e3bfb53c51391978bf741e62ad11a15570
BadWaka <1456683844@qq.com>
2017-05-09 21:42:06 +0800

    Slider存在边界超出的问题

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -29,17 +29,27 @@ export default class Index extends Component {
             slidesToScroll: 1
         };
 
+        const imgArr = [
+            'http://img.hb.aicdn.com/ddc7a18f7a6951eb10e675b892bf069fdee63136133f58-IcaOih_fw658',
+            'http://img.hb.aicdn.com/12a96b5c549bad2ed4e5544a36ab07fb8506c55d1cf2c-0NXOMB_fw658',
+            'http://img.hb.aicdn.com/fd264e4afb1d40b544945d243e2e7b4453faccc224c10-xrOyy1_fw658'
+        ];
+
         return (
             <section className={style.index}>
                 <Header/>
                 <Slider {...settings}>
-                    <div><h3>1</h3></div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
-                    <div><h3>5</h3></div>
-                    <div><h3>6</h3></div>
+                    {imgArr.map((imgUrl, index) => {
+                        return <div
+                            className={style.imgWrapper}
+                            key={index}
+                            style={{
+                                backgroundImage: 'url(' + imgUrl + ')'
+                            }}>
+                        </div>
+                    })}
                 </Slider>
+
             </section>
         );
     }
