f443319b9c97c0bbe22f75ff4360ddf43f428d63
BadWaka <1456683844@qq.com>
2017-05-15 21:28:17 +0800

    自己配置该死的eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 /**
  * Created by BadWaka on 04/05/2017.
  */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import style from './App.scss';
-
-// 业务组件
-import {AlreadyAppleFor, ChangeRelatedCertificate, CopyrightProtectionCertificate, NotApplyFor} from './businessComponents/CopyrightProtectionModals/';
 
 // 个人组件
 import {
-    Header, // 头部
-    Index, // 主页
-    Slider, // 轮播图
+  Index, // 主页
 } from './components';
 
 export default class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <Index/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <section className={style.app}>
+        <Index />
+      </section>
+    );
+  }
 }
